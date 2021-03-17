5e9ee85d8bd4042609479ab966a467ebcc5f2e16
Natália Carreras <62567966+nataliaqmc@users.noreply.github.com>
2021-03-17 16:25:09 -0300

    Add files via upload

---
+++ VaiMalte/src/br/edu/insper/desagil/aps1/Testador.java
@@ -0,0 +1,27 @@
+package br.edu.insper.desagil.aps1;
+
+public class Testador {
+	public boolean example() {
+	    return true;
+	}
+	public boolean testeA() {
+	    return false;
+	}
+
+	public boolean testeB() {
+	    return false;
+	}
+
+	public boolean testeC() {
+	    return false;
+	}
+
+	public boolean testeD() {
+	    return false;
+	}
+
+	public boolean testeE() {
+	    return false;
+	}
+
+}
