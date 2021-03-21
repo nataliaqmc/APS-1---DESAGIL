23db9dfea7391f016becd6975a9a277a5a08a767
Natália Carreras <62567966+nataliaqmc@users.noreply.github.com>
2021-03-21 17:22:31 -0300

    Update Testador.java

--- VaiMalte/VaiMalte/src/br/edu/insper/desagil/vaimalte/Testador.java
+++ VaiMalte/VaiMalte/src/br/edu/insper/desagil/vaimalte/Testador.java
@@ -2,14 +2,42 @@ package br.edu.insper.desagil.vaimalte;
 
 public class Testador {
 	public boolean testeA() {
-	    return false;
+		
+		Carrinho carrinho = new Carrinho ();
+		Caixa caixa = new Caixa ();
+
+		if (caixa.total(carrinho) == 0) {
+			return true;
+		}
+	    	return false;
 	}
 
 	public boolean testeB() {
+		Carrinho carrinho = new Carrinho();
+		Produto produto = new Produto(4,"Banana",2);
+		
+		carrinho.insere(produto);
+		
+		Caixa caixa = new Caixa();
+		
+		if (caixa.total(carrinho) == 2.00) {
+			return true;
+		}
 		return false;
 	}
 
 	public boolean testeC() {
+		Carrinho carrinho = new Carrinho();
+		Produto produto = new Produto(4,"Banana",12.00);
+		
+		carrinho.insere(produto);
+		
+		Caixa caixa = new Caixa();
+		caixa.insereDesconto(produto, 20);
+		
+		if (caixa.total(carrinho)== 9.60) {
+			return true;
+		}
 		return false;
 	}
 
