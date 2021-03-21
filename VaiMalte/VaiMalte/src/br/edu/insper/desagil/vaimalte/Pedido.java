dc1ea6debf9c332bcb8df63259b89b7002225b79
Natália Carreras <62567966+nataliaqmc@users.noreply.github.com>
2021-03-21 17:18:50 -0300

    Update Pedido.java

--- VaiMalte/VaiMalte/src/br/edu/insper/desagil/vaimalte/Pedido.java
+++ VaiMalte/VaiMalte/src/br/edu/insper/desagil/vaimalte/Pedido.java
@@ -2,4 +2,29 @@ package br.edu.insper.desagil.vaimalte;
 
 public class Pedido {
 
+	private Produto produto;
+	private int quantidade;
+
+	public Pedido(Produto produto) {
+		super();
+		this.produto = produto;
+		this.quantidade = 1;
+	}
+	
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public void incrementa () {
+		this.quantidade ++;
+	}
+	
+	public double precoCheio () {
+		return quantidade * produto.getPreco();
+	}
+
 }
